a71c9ec8514ccc6ac0134d0756c1c55ccc6ef109
youpy <youpy@buycheapviagraonlinenow.com>
2010-03-09 01:45:29 +0900

    add exapmple

---
+++ example/js_minifier.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/enb ruby
+
+require 'rubygems'
+require 'grope'
+
+if ARGV.size != 1
+  warn "usage: #{$0} /path/to/js/to/minify.js"
+  exit 1
+end
+
+js_filename = ARGV.shift
+
+env = Grope::Env.open('http://fmarcia.info/jsmin/test.html')
+env.xpath('//textarea')[1].value = open(js_filename).read
+env.document.getElementById('go').click()
+puts env.xpath('//textarea')[2].value
