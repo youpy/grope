758dd09d65905d7fc1d0c8ec0c633eb55c51d4b9
youpy <youpy@buycheapviagraonlinenow.com>
2010-03-12 17:43:27 +0900

    more verbose output

--- lib/grope/web_resource_load_delegate.rb
+++ lib/grope/web_resource_load_delegate.rb
@@ -11,7 +11,7 @@ module Grope
 
       cookies = cookie_storage.cookies_for_url(request.URL.to_s)
       if cookies.size > 0
-        warn '*** send cookie for %s, %s' % [request.URL.to_s, cookies]
+        warn "*** send cookie for %s ***\n%s" % [request.URL.to_s, cookies]
         cookie_fields = NSHTTPCookie.requestHeaderFieldsWithCookies(cookies)
         request.setAllHTTPHeaderFields(cookie_fields)
       end
@@ -30,9 +30,20 @@ module Grope
       url = response.URL
 
       NSHTTPCookie.cookiesWithResponseHeaderFields_forURL(headers, url).each do |cookie|
-        warn '*** store cookie for %s, %s' % [response.URL.to_s, cookie]
+        warn "*** store cookie for %s ***\n%s" % [response.URL.to_s, cookie]
         cookie_storage.set_cookie(cookie)
       end
     end
+  end
+end
+
+class NSHTTPCookie
+  def to_s
+    value = self.value
+    if value.size > 50
+      value = value[0, 47] + '...'
+    end
+
+    "%s=%s\t%s\t%s\t%s\n" % [name, value, domain, path, expiresDate]
   end
 end
