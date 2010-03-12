935d6ebbc3b1704f3e93ad109efd137246e0ceed
youpy <youpy@buycheapviagraonlinenow.com>
2010-03-13 06:38:35 +0900

    add NSHTTPCookieWrapper

--- lib/grope/web_resource_load_delegate.rb
+++ lib/grope/web_resource_load_delegate.rb
@@ -10,8 +10,9 @@ module Grope
           set_cookies(redirect_response)
         end
 
-        cookies = cookie_storage.cookies(URI(request.URL.to_s))
+        cookies = cookie_storage.cookies(URI(request.URL.to_s)).map {|wrapper| wrapper.cookie}
         if cookies.size > 0
+          #warn "*** send cookie for %s ***\n%s" % [request.URL.to_s, cookies]
           cookie_fields = NSHTTPCookie.requestHeaderFieldsWithCookies(cookies)
           request.setAllHTTPHeaderFields(cookie_fields)
         end
@@ -33,14 +34,52 @@ module Grope
       url = response.URL
 
       NSHTTPCookie.cookiesWithResponseHeaderFields_forURL(headers, url).each do |cookie|
-        cookie_storage.add(URI(url.to_s), cookie)
+        cookie_storage.add(URI(url.to_s), NSHTTPCookieWrapper.new(cookie))
+        #warn "*** store cookie for %s ***\n%s" % [response.URL.to_s, cookie]
       end
     end
   end
 end
 
 class NSHTTPCookie
+  def to_s
+    value = self.value
+    if value.size > 50
+      value = value[0, 47] + '...'
+    end
+
+    "%s=%s\t%s\t%s\t%s\n" % [name, value, domain, path, expiresDate, isSecure]
+  end
+end
+
+class NSHTTPCookieWrapper
+  attr_reader :cookie
+
+  def initialize(cookie)
+    @cookie = cookie
+  end
+
+  def domain
+    cookie.domain && cookie.domain.to_s.sub(/^\./, '')
+  end
+
+  def path
+    cookie.path && cookie.path.to_s
+  end
+
+  def name
+    cookie.name.to_s
+  end
+
+  def value
+    cookie.value
+  end
+
+  def secure
+    cookie.isSecure == true.to_ns ? true : false
+  end
+
   def expires
-    Time.at(expiresDate.timeIntervalSince1970.to_i)
+    cookie.expiresDate && Time.at(cookie.expiresDate.timeIntervalSince1970.to_i)
   end
 end
