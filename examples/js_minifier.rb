a0064fc2eaf54e6772c6d4fbe5e4f934b762e5b4
youpy <youpy@buycheapviagraonlinenow.com>
2010-03-15 16:38:32 +0900

    reflect API changes

--- examples/js_minifier.rb
+++ examples/js_minifier.rb
@@ -12,6 +12,6 @@ js_filename = ARGV.shift
 
 env = Grope::Env.new
 env.load('http://fmarcia.info/jsmin/test.html')
-env.xpath('//textarea')[1].value = open(js_filename).read
+env.all('//textarea')[1].value = open(js_filename).read
 env.document.getElementById('go').click
-puts env.xpath('//textarea')[2].value
+puts env.all('//textarea')[2].value
