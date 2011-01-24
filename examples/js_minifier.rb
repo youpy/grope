e969c535c0250fcc24e668ad25eafd45232f37bc
youpy <youpy@buycheapviagraonlinenow.com>
2011-01-24 17:00:21 +0900

    minify.js can read javascript from stdin

--- examples/js_minifier.rb
+++ examples/js_minifier.rb
@@ -3,15 +3,11 @@
 require 'rubygems'
 require 'grope'
 
-if ARGV.size != 1
-  warn "usage: #{$0} /path/to/js/to/minify.js"
-  exit 1
-end
-
 js_filename = ARGV.shift
+js = (js_filename ? open(js_filename) : STDIN).read
 
 env = Grope::Env.new
 env.load('http://fmarcia.info/jsmin/test.html')
-env.all('//textarea')[1].value = open(js_filename).read
+env.all('//textarea')[1].value = js
 env.document.getElementById('go').click
 puts env.all('//textarea')[2].value
